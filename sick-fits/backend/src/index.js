49bfc3fdf2a845e9d9cd7f6985b689a4220ed019
Evan <evan@evanjp.com>
2020-11-20 21:50:33 -0800

    GraphQL Yoga Express server running locally

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO: Use express middleware to handle cookies (JWT)
+// TODO: Use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  (serverInfo) => {
+    console.log(`Server is running at http://localhost:${serverInfo.port}`);
+  }
+);
