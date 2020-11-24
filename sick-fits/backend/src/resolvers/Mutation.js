47762df09cc65dce7c3e1cc44dc9a7ae6e7d1567
Evan <evan@evanjp.com>
2020-11-23 19:37:40 -0800

    testing out query/mutation resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutation = {};
+const Mutation = {
+  createDog(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    const dog = { name: args.name };
+    global.dogs.push(dog);
+    return dog;
+  },
+};
 
 module.exports = Mutation;
